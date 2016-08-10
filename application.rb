941b07cd232c7186a8fed152405d20629b713885
lgants <lgants@gmail.com>
2016-08-10 16:22:13 -0400

    rack intro lab complete -ltg

--- application.rb
+++ application.rb
@@ -1,10 +1,9 @@
+#code in class Application will run whenever there is a request received
 class Application
-
+  #env holds all the request info
   def call(env)
     resp = Rack::Response.new
-    resp.write "Hello, World"
+    resp.write "Hello, my name is Logan"
     resp.finish
   end
-
 end
-
--- config.ru
+++ config.ru
@@ -1,3 +1,6 @@
+#set up an HTTP web server that will receive request, send it to call method, then serve response to browser
+#run this code with rackup config.ru
+
 require_relative "./application.rb"
 
 run Application.new
